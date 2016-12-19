d6e6047bfc510fbc6b7e9b576347f107fd843106
Jacob Kaplan <jacobkap@sas.upenn.edu>
2016-12-18 19:07:11 -0800

    minor change to master_column_dictionaru

--- R_code/main.R
+++ R_code/main.R
@@ -1,5 +1,6 @@
 # Combine files into master file
 
+setwd("C:/Users/user/Dropbox/R_project/crime_data")
 load("crosswalk.rda")
 load("leoka.rda")
 load("supplementary_homicide.rda")
@@ -15,9 +16,20 @@ master_ucr <- master_ucr[, c(1:3, 66:103, 64:65, 4:63, 104:147)]
 master_ucr <- dplyr::arrange(master_ucr, year, ORI)
 save(master_ucr, file = "master_ucr.rda", compress = "xz")
 
-master_column_dictionary <- data.frame(column = "",
-                                       source = "")
-crosswalk_columns <- data.frame(column = "1-27",
-                                source = "crosswalk")
+
+master_column_dictionary <- data.frame(column = "1-27",
+                                       source = "crosswalk")
+leoka_column <- data.frame(column = "28-41",
+                           source = "LEOKA")
+offenses_column <- data.frame(column = "42-103",
+                           source = "offenses_and_clearances")
+shr_column <- data.frame(column = "104-147",
+                              source = "supplementary_homicide_report")
 master_column_dictionary <- rbind(master_column_dictionary,
-                                  crosswalk_columns)
+                                  leoka_column,
+                                  offenses_column,
+                                  shr_column)
+master_column_dictionary$info <- ""
+master_column_dictionary$info[1] <- "Column dictionary for UCR master dataset"
+save(master_column_dictionary, file = "master_column_dictionary.rda",
+     compress = "xz")
