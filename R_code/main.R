352cb6878884949979b53464db1d2c6e2d2c996d
Jacob Kaplan <jacobkap@sas.upenn.edu>
2016-12-20 18:19:53 -0800

    fix naming

--- R_code/main.R
+++ R_code/main.R
@@ -1,31 +1,33 @@
 # Combine files into master file
 
 setwd("C:/Users/user/Dropbox/R_project/crime_data")
-load("crosswalk.rda")
-load("leoka.rda")
-load("supplementary_homicide.rda")
-load("ucr_offenses.rda")
+load("police_agency_identifier_crosswalk.rda")
+load("ucr_police_employee_leoka.rda")
+load("ucr_supplementary_homicide_report.rda")
+load("ucr_offenses_clearances.rda")
 
 
-master_ucr <- dplyr::left_join(ucr_offenses, crosswalk)
-master_ucr <- dplyr::left_join(master_ucr, leoka)
-master_ucr <- dplyr::left_join(master_ucr, supplementary_homicide)
-master_ucr <- master_ucr[-grep("99999", master_ucr$ORI),]
-rownames(master_ucr) <- 1:nrow(master_ucr)
-master_ucr <- master_ucr[, c(1:3, 66:103, 64:65, 4:63, 104:147)]
-master_ucr <- dplyr::arrange(master_ucr, year, ORI)
-save(master_ucr, file = "master_ucr.rda", compress = "xz")
+master_crime <- dplyr::left_join(ucr_offenses_clearances,
+                                 police_agency_identifier_crosswalk)
+master_crime <- dplyr::left_join(master_crime,
+                                 ucr_police_employee_leoka)
+master_crime <- dplyr::left_join(master_crime,
+                                 ucr_supplementary_homicide_report)
+rownames(master_crime) <- 1:nrow(master_crime)
+master_crime <- master_crime[, c(1:3, 66:103, 64:65, 4:63, 104:147)]
+master_crime <- dplyr::arrange(master_crime, year, ORI)
+save(master_crime, file = "master_crime.rda", compress = "xz")
 
 master_column_dictionary <- data.frame(column = "1-27",
-                                       source = "crosswalk")
-leoka_column <- data.frame(column = "28-41",
-                           source = "LEOKA")
+                  source = "police_agency_identifier_crosswalk")
+ucr_police_employee_leoka_column <- data.frame(column = "28-41",
+                           source = "ucr_police_employee_leoka")
 offenses_column <- data.frame(column = "42-103",
                               source = "offenses_and_clearances")
 shr_column <- data.frame(column = "104-147",
-                         source = "supplementary_homicide_report")
+                         source = "ucr_supplementary_homicide_report_report")
 master_column_dictionary <- rbind(master_column_dictionary,
-                                  leoka_column,
+                                  ucr_police_employee_leoka_column,
                                   offenses_column,
                                   shr_column)
 master_column_dictionary$info <- ""
@@ -36,55 +38,65 @@ save(master_column_dictionary, file = "master_column_dictionary.rda",
 
 # Save all as compress excel files
 setwd("C:/Users/user/Dropbox/R_project/crime_data/clean_data/excel")
-write.csv(crosswalk, xzfile("crosswalk.csv.xz"))
+write.csv(police_agency_identifier_crosswalk,
+          file = "police_agency_identifier_crosswalk.csv")
 write.csv(master_column_dictionary,
-          xzfile("master_column_dictionary.csv.xz"))
-write.csv(supplementary_homicide,
-          xzfile("supplementary_homicide.csv.xz"))
-write.csv(leoka, xzfile("leoka.csv.xz"))
-write.csv(ucr_offenses,
-          xzfile("ucr_offenses.csv.xz"))
-write.csv(master_ucr,
-          xzfile("master_ucr.csv.xz"))
+          file = "master_column_dictionary.csv")
+write.csv(ucr_supplementary_homicide_report,
+          file = "ucr_supplementary_homicide_report.csv")
+write.csv(ucr_police_employee_leoka, file = "ucr_police_employee_leoka.csv")
+write.csv(ucr_offenses_clearances,
+          file = "ucr_offenses_clearances.csv")
+write.csv(master_crime,
+          file = "master_crime.csv")
 
 
 # Save as .dta for stata
 setwd("C:/Users/user/Dropbox/R_project/crime_data/clean_data/stata")
-names(supplementary_homicide) <- gsub("SHR_babysitter_or_parent_kills_child",
-                                      "SHR_babysitter_or_parent",
-                                      names(supplementary_homicide))
-names(master_ucr) <- gsub("SHR_babysitter_or_parent_kills_child",
-                                      "SHR_babysitter_or_parent",
-                                      names(master_ucr))
-haven::write_dta(crosswalk, path = "crosswalk.dta")
+names(ucr_supplementary_homicide_report) <-
+           gsub("SHR_babysitter_or_parent_kills_child",
+          "SHR_babysitter_or_parent",
+          names(ucr_supplementary_homicide_report))
+names(master_crime) <- gsub("SHR_babysitter_or_parent_kills_child",
+                      "SHR_babysitter_or_parent",
+                      names(master_crime))
+haven::write_dta(police_agency_identifier_crosswalk,
+                 path = "police_agency_identifier_crosswalk.dta")
 haven::write_dta(master_column_dictionary,
                  path = "master_column_dictionary.dta")
-haven::write_dta(supplementary_homicide,
-                 path = "supplementary_homicide.dta")
-haven::write_dta(leoka, path = "leoka.dta")
-haven::write_dta(ucr_offenses, path = "ucr_offenses.dta")
-haven::write_dta(master_ucr, path = "master_ucr.dta")
+haven::write_dta(ucr_supplementary_homicide_report,
+                 path = "ucr_supplementary_homicide_report.dta")
+haven::write_dta(ucr_police_employee_leoka,
+                 path = "ucr_police_employee_leoka.dta")
+haven::write_dta(ucr_offenses_clearances,
+                 path = "ucr_offenses_clearances.dta")
+haven::write_dta(master_crime, path = "master_crime.dta")
 
 
 
 # Save as .sas for sas
 setwd("C:/Users/user/Dropbox/R_project/crime_data/clean_data/sas")
-haven::write_sas(crosswalk, path = "crosswalk.sas")
+haven::write_sas(police_agency_identifier_crosswalk,
+                 path = "police_agency_identifier_crosswalk.sas")
 haven::write_sas(master_column_dictionary,
                  path = "master_column_dictionary.sas")
-haven::write_sas(supplementary_homicide,
-                 path = "supplementary_homicide.sas")
-haven::write_sas(leoka, path = "leoka.sas")
-haven::write_sas(ucr_offenses, path = "ucr_offenses.sas")
-haven::write_sas(master_ucr, path = "master_ucr.sas")
+haven::write_sas(ucr_supplementary_homicide_report,
+                 path = "ucr_supplementary_homicide_report.sas")
+haven::write_sas(ucr_police_employee_leoka,
+                 path = "ucr_police_employee_leoka.sas")
+haven::write_sas(ucr_offenses_clearances, path = "ucr_offenses_clearances.sas")
+haven::write_sas(master_crime, path = "master_crime.sas")
 
 # Save as .sav for spss
 setwd("C:/Users/user/Dropbox/R_project/crime_data/clean_data/spss")
-haven::write_sav(crosswalk, path = "crosswalk.sav")
+haven::write_sav(police_agency_identifier_crosswalk,
+                 path = "police_agency_identifier_crosswalk.sav")
 haven::write_sav(master_column_dictionary,
                  path = "master_column_dictionary.sav")
-haven::write_sav(supplementary_homicide,
-                 path = "supplementary_homicide.sav")
-haven::write_sav(leoka, path = "leoka.sav")
-haven::write_sav(ucr_offenses, path = "ucr_offenses.sav")
-haven::write_sav(master_ucr, path = "master_ucr.sav")
+haven::write_sav(ucr_supplementary_homicide_report,
+                 path = "ucr_supplementary_homicide_report.sav")
+haven::write_sav(ucr_police_employee_leoka,
+                 path = "ucr_police_employee_leoka.sav")
+haven::write_sav(ucr_offenses_clearances,
+                 path = "ucr_offenses_clearances.sav")
+haven::write_sav(master_crime, path = "master_crime.sav")
