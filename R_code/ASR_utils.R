8b731e3f3664c03d979a9a3c46d6bb79c8b99ec3
jacobkap <jkkaplan6@gmail.com>
2018-02-25 12:29:05 -0500

    Tests that my yearly aggregator is same as ICPSR's is mostly identical. Issues seem to be in data, not in code.

--- R_code/ASR_utils.R
+++ R_code/ASR_utils.R
@@ -1,55 +1,8 @@
-# Reads and cleans Arrest by Age, Sex, and Race (ASR) data from
-# the FBI's UCR.
-
-library(dplyr)
 devtools::install_github("jacobkap/asciiSetupReader")
 library(asciiSetupReader)
 library(data.table)
 library(stringr)
-setwd("C:/Users/user/Dropbox/R_project/crime_data/raw_data/ASR")
-
-# SC00704 TX15525
-for (year in 1980:2014) {
-  message(year)
-
-  ASR <- spss_ascii_reader(paste0("UCR_arrests_by_age_sex_race_",
-                                  year, ".txt"),
-                           paste0("UCR_arrests_by_age_sex_race_",
-                                  year, ".sps"),
-                           keep_columns = arrest_cols,
-                           value_label_fix = FALSE)
-  ASR$OFFENSE_CODE <- str_replace_all(ASR$OFFENSE_CODE, offense_codes)
-  ASR[4:59] <- sapply(ASR[4:59], function(x) remove_missing(x))
-  ASR <- ASR %>% group_by(ORIGINATING_AGENCY_IDENTIFIER_CODE,
-                          OFFENSE_CODE,
-                          YEAR) %>%
-    summarise_all(funs(sum(., na.rm = TRUE)))
-
-  ASR_year <- spss_ascii_reader(paste0("UCR_arrests_by_age_sex_race_yearly_",
-                                       year, ".txt"),
-                                paste0("UCR_arrests_by_age_sex_race_yearly_",
-                                       year, ".sps"),
-                                keep_columns = arrest_cols,
-                                value_label_fix = FALSE)
-
-
-  ASR_year <- ASR_year[, names(ASR)]
-  ASR_year[4:59] <- sapply(ASR_year[4:59], function(x) remove_missing(x))
-  ASR_year[4:59] <- sapply(ASR_year[4:59], function(x) NA_to_zero(x))
-  ASR_year$OFFENSE_CODE <- as.character(ASR_year$OFFENSE_CODE)
-  ASR_year$ORIGINATING_AGENCY_IDENTIFIER_CODE <- as.character(ASR_year$ORIGINATING_AGENCY_IDENTIFIER_CODE)
-
-  ASR <- ASR[!is.na(ASR$OFFENSE_CODE), ]
-  ASR_year <- ASR_year[!is.na(ASR_year$OFFENSE_CODE), ]
-  ASR <- ASR[ASR$ORIGINATING_AGENCY_IDENTIFIER_CODE %in% ASR_year$ORIGINATING_AGENCY_IDENTIFIER_CODE, ]
-  ASR$OFFENSE_CODE <- NULL
-  ASR_year$OFFENSE_CODE <- NULL
-
-  message(all.equal(ASR, ASR_year))
-
-  rm(ASR); rm(ASR_year); gc(); Sys.sleep(10)
-}
-
+library(dplyr)
 
 offense_codes <- c("^01A$" = "Murder and non-negligent manslaughter",
                    "^01B$" = "Manslaughter by negligence",
@@ -96,6 +49,51 @@ offense_codes <- c("^01A$" = "Murder and non-negligent manslaughter",
                    "^29$"  = "Runaways",
                    "^998$" = NA)
 
+offense_codes_yearly <- c("^11$" = "Murder and non-negligent manslaughter",
+                          "^12$" = "Manslaughter by negligence",
+                          "^20$"  = "Forcible rape",
+                          "^30$"  = "Robbery",
+                          "^40$"  = "Aggravated assault",
+                          "^50$"  = "Burglary-breaking or entering",
+                          "^60$"  = "Larceny-theft (not motor vehicle)",
+                          "^70$"  = "Motor vehicle theft",
+                          "^80$"  = "Other assaults",
+                          "^90$"  = "Arson",
+                          "^100$"  = "Forgery and counterfeiting",
+                          "^110$"  = "Fraud",
+                          "^120$"  = "Embezzlement",
+                          "^130$"  = "Stolen property-buy, receive, poss.",
+                          "^140$"  = "Vandalism",
+                          "^150$"  = "Weapons-carry, posses, etc.",
+                          "^160$"  = "Prostitution and commercialized vice",
+                          "^170$"  = "Sex offenses (not rape or prostitution)",
+                          "^18$"  = "Total drug abuse violations",
+                          "^180$" = "Sale/manufacture (subtotal)",
+                          "^185$" = "Possession (subtotal)",
+                          "^181$" = "Sale: Opium, coke, and their derivatives",
+                          "^182$" = "Sale: Marijuana",
+                          "^183$" = "Sale: Truly addicting synthetic narcotics",
+                          "^184$" = "Sale: Other dangerous non-narc drugs",
+                          "^186$" = "Possession: Opium, coke, and their derivatives",
+                          "^187$" = "Possession: Marijuana",
+                          "^188$" = "Possession: Truly addicting synthetic narcotics",
+                          "^189$" = "Possession: Other dangerous non-narc drugs",
+                          "^190$"  = "Gambling (total)",
+                          "^191$" = "Bookmaking (horse and sports)",
+                          "^192$" = "Number and lottery",
+                          "^193$" = "All other gambling",
+                          "^200$"  = "Offenses against family and children",
+                          "^210$"  = "Driving under the influence",
+                          "^220$"  = "Liquor laws",
+                          "^230$"  = "Drunkenness",
+                          "^240$"  = "Disorderly conduct",
+                          "^250$"  = "Vagrancy",
+                          "^260$"  = "All other offenses (not traffic)",
+                          "^270$"  = "Suspicion",
+                          "^280$"  = "Curfew and loitering violations",
+                          "^290$"  = "Runaways",
+                          "^98$" = NA)
+
 arrest_cols <- c("ORI", "YEAR", "OFFENSE",
                  "M0_9", "M10_12", "M13_14", "M15",
                  "M16", "M17", "M18", "M19", "M20",
@@ -111,10 +109,10 @@ arrest_cols <- c("ORI", "YEAR", "OFFENSE",
                  "AH", "AN", "JH","JN")
 
 remove_missing <- function(column) {
-  column[column %in% c(99999, 99998, 10000, 20000, 30000,
-                       10001, 10002)] <- NA
+  column[column %in% c(99999, 99998)] <- NA
   return(column)
 }
+
 
 NA_to_zero <- function(column) {
   column[is.na(column)] <- 0
---
+++ R_code/ASR_yearly_test.R
@@ -0,0 +1,45 @@
+# Reads and cleans Arrest by Age, Sex, and Race (ASR) data from
+# the FBI's UCR.
+
+setwd("C:/Users/user/Dropbox/R_project/crime_data/raw_data/ASR")
+
+
+# Year 1989, ORIs AZ00310 and MD00204 are in yearly but not monthly data
+# Year 2001, ORIs are in "AL02306" "AL02805" "AL02814" are in monthly but
+# not yearly data
+for (year in 1980:2014) {
+  message(year)
+
+  ASR <- spss_ascii_reader(paste0("UCR_arrests_by_age_sex_race_",
+                                  year, ".txt"),
+                           paste0("UCR_arrests_by_age_sex_race_",
+                                  year, ".sps"),
+                           keep_columns = arrest_cols,
+                           value_label_fix = FALSE)
+  ASR$OFFENSE_CODE <- str_replace_all(ASR$OFFENSE_CODE, offense_codes)
+  ASR[4:59] <- sapply(ASR[4:59], function(x) remove_missing(x))
+  ASR <- ASR %>% group_by(ORIGINATING_AGENCY_IDENTIFIER_CODE,
+                          OFFENSE_CODE,
+                          YEAR) %>%
+    summarise_all(funs(sum(., na.rm = TRUE)))
+  ASR <- ASR[!is.na(ASR$OFFENSE_CODE), ]
+
+  ASR_year <- spss_ascii_reader(paste0("UCR_arrests_by_age_sex_race_yearly_",
+                                       year, ".txt"),
+                                paste0("UCR_arrests_by_age_sex_race_yearly_",
+                                       year, ".sps"),
+                                keep_columns = arrest_cols,
+                                value_label_fix = FALSE)
+  ASR_year$OFFENSE_CODE <- str_replace_all(ASR_year$OFFENSE_CODE, offense_codes_yearly)
+  ASR_year <- ASR_year[, names(ASR)]
+  ASR_year[4:59] <- sapply(ASR_year[4:59], function(x) remove_missing(x))
+  ASR_year[4:59] <- sapply(ASR_year[4:59], function(x) NA_to_zero(x))
+  ASR_year <- ASR_year[!is.na(ASR_year$OFFENSE_CODE), ]
+
+
+  ASR <- ASR[ASR$ORIGINATING_AGENCY_IDENTIFIER_CODE %in% ASR_year$ORIGINATING_AGENCY_IDENTIFIER_CODE, ]
+  ASR_year <- ASR_year[ASR_year$ORIGINATING_AGENCY_IDENTIFIER_CODE %in% ASR$ORIGINATING_AGENCY_IDENTIFIER_CODE, ]
+  message("Only common ORIs:    ", nrow(ASR), " ", nrow(ASR_year), " ", all.equal(ASR, ASR_year))
+
+  rm(ASR); rm(ASR_year); gc(); Sys.sleep(10)
+}
